10c102d6c5e1673ebae185d3e8c2c373f7b2385b
Yannick Lang <layaxx@gmx.de>
2023-03-06 21:42:01 +0100

    add distance data and improvements

---
+++ data/src/distances.R
@@ -0,0 +1,48 @@
+library(jsonify)
+library(ggplot2)
+
+setwd("C:\\Users\\layax\\Documents\\Uni\\s7_bachelorarbeit\\paper\\data\\src")
+
+distances <- jsonify::from_json( "distances.json" )
+
+View(disan)
+
+
+ggplot(distances ,aes(x = distance)) + 
+  geom_histogram(aes(y = stat(density)),binwidth = 10  ) + 
+  geom_vline(xintercept =  median(distances$distance))
+
+
+min(distances$distance)
+
+
+
+
+
+distance_in_level <- c(189,
+                       127,
+                       286,
+                       158,
+                       166,
+                       130,
+                       213,
+                       298,
+                       498,
+                       114,
+                       222,
+                       344,
+                       211,
+                       245,
+                       291,
+                       268,
+                       167,
+                       157,
+                       161,
+                       104,
+                       192)
+
+mean(distance_in_level)
+
+median(distance_in_level)
+
+
